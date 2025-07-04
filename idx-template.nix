0eca700ae67cd4fa77b3dc02283abc5f0831ada6
esther <estherlloyd@google.com>
2025-07-04 09:06:18 +0000

    fix: command

--- idx-template.nix
+++ idx-template.nix
@@ -6,6 +6,6 @@
     cp -rf ${./.} "$out"
     chmod -R +wx "$out"
     rm -rf "$out/.git" "$out/idx-template".{nix,json}
-    cd "$out" && npm install -g @google/gemini-cli 
+    cd "$out"; npm install -g @google/gemini-cli 
   '';
 }
